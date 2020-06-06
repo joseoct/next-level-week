6d8cd2b678e3d77872b6aa8e80c637b8856facd0
Jose <joseoctavioconti@hotmail.com>
2020-06-06 19:53:48 -0300

    Validando entrada de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import { errors } from 'celebrate';
 import cors from 'cors';
 import routes from './routes';
 import path from 'path';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
